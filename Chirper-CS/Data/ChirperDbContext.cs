e2cce929150dbb29bbd88528c2a034a7763dbfee
ngcsontrh <trhsonngoc12@gmail.com>
2024-06-01 00:59:59 +0700

    Add project files.

---
+++ Chirper-CS/Data/ChirperDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Chirper_CS.Data
+{
+    public class ChirperDbContext  : DbContext
+    {
+        public ChirperDbContext(DbContextOptions<ChirperDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
